3035b4d8151c01224518dc14dd07d148863219f3
Elizabeth <elizabeth.valenti@selu.edu>
2025-04-27 19:22:57 -0500

    Update MainModule.py

--- MainModule.py
+++ MainModule.py
@@ -32,6 +32,8 @@ if __name__ == "__main__":
    import os
 
 import ParentChild1
+import ParentChild2
+
 
 #%% USER INTERFACE              ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
@@ -64,6 +66,31 @@ def f():
 
     log_test = ParentChild1.CustomerAnalytics('badfile.lol')
     test_df = log_test.read_csv()
+
+    array = ParentChild2.Inventory.readData('Input/data.csv')
+    ParentChild2.Inventory.convert_to_df(array)
+    
+    test = ParentChild2.InventoryAnalytics('test_pickle.pkl')
+    testResult = test.readPickle()
+
+    events, conditions = test.get_events(Party_size=2, Arrival_Time = 'eval:>= 12')
+    
+    probabilities = test.get_probabilities(events[0], events[1], conditions[0], conditions[1])
+
+    test.get_all_column_stats(testResult)
+
+    test.get_events(Price=lambda x: x > 50, Quantity="eval:< 100")
+    test.Vector_Ops('Party_size', 'Bill_Amount')
+    test.unique_vals('Party_size', 'Arrival_Time', 'Duration_of_Stay', 'Bill_Amount')
+
+#%% SELF-RUN                   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#Main Self-run block
+if __name__ == "__main__":
+    
+    #print(f"\"{module_name_gl}\" module begins.")
+    
+    f()
+
 
 #%% SELF-RUN                   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #Main Self-run block
